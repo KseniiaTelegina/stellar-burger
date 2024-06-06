721fea370262bb1c0c81af28cface7b5cd8931ec
Kseniia Telegina <ksusha2993@gmail.com>
2024-06-06 18:24:27 +0300

    начало начал

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,14 +1,126 @@
+import React from 'react';
+import { useLocation } from 'react-router-dom';
 import { ConstructorPage } from '@pages';
+import { Feed } from '@pages';
+import { NotFound404 } from '@pages';
+import { Login } from '@pages';
+import { Register } from '@pages';
+import { ForgotPassword } from '@pages';
+import { ResetPassword } from '@pages';
+import { Profile } from '@pages';
+import { ProfileOrders } from '@pages';
 import '../../index.css';
 import styles from './app.module.css';
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useNavigate
+} from 'react-router-dom';
 import { AppHeader } from '@components';
+import { Modal } from '@components';
+import { OrderInfo } from '@components';
+import { IngredientDetails } from '@components';
+import { ProtectedRoute } from '../protected-route/ProtectedRoute';
 
-const App = () => (
-  <div className={styles.app}>
-    <AppHeader />
-    <ConstructorPage />
-  </div>
-);
+const App = () => {
+  const location = useLocation();
+  const backgroundLocation = location.state?.backgroundLocation;
+  const navigate = useNavigate();
+  const handleModalClose = () => {
+    navigate(-1);
+  };
+
+  return (
+    <Router>
+      <div className={styles.app}>
+        <AppHeader />
+        <Routes location={backgroundLocation || location}>
+          <Route path='/' element={<ConstructorPage />} />
+          <Route path='/feed' element={<Feed />} />
+          <Route path='*' element={<NotFound404 />} />
+          <Route
+            path='/login'
+            element={
+              <ProtectedRoute onlyUnAuth>
+                <Login />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path='/register'
+            element={
+              <ProtectedRoute onlyUnAuth>
+                <Register />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path='/forgot-password'
+            element={
+              <ProtectedRoute onlyUnAuth>
+                <ForgotPassword />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path='/reset-password'
+            element={
+              <ProtectedRoute onlyUnAuth>
+                <ResetPassword />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path='/profile'
+            element={
+              <ProtectedRoute onlyUnAuth>
+                <Profile />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path='/profile/orders'
+            element={
+              <ProtectedRoute onlyUnAuth>
+                <ProfileOrders />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+        {backgroundLocation && (
+          <Routes>
+            <Route
+              path='/feed/:number'
+              element={
+                <Modal title='Order Information' onClose={handleModalClose}>
+                  <OrderInfo />
+                </Modal>
+              }
+            />
+            <Route
+              path='/ingredients/:id'
+              element={
+                <Modal title='Ingredient Details' onClose={handleModalClose}>
+                  <IngredientDetails />
+                </Modal>
+              }
+            />
+            <Route
+              path='/profile/orders/:number'
+              element={
+                <ProtectedRoute onlyUnAuth>
+                  <Modal title='Profile Order' onClose={handleModalClose}>
+                    <OrderInfo />
+                  </Modal>
+                </ProtectedRoute>
+              }
+            />
+          </Routes>
+        )}
+      </div>
+    </Router>
+  );
+};
 
 export default App;
