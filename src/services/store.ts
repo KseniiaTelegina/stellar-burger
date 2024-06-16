9e4893245b0df78d269a242418e8162efe4874e7
KseniiaTelegina <146709530+KseniiaTelegina@users.noreply.github.com>
2024-06-16 12:33:46 +0300

    Update store.ts

--- src/services/store.ts
+++ src/services/store.ts
@@ -1,40 +1,23 @@
 import { configureStore } from '@reduxjs/toolkit';
+
 import {
   TypedUseSelectorHook,
   useDispatch as dispatchHook,
   useSelector as selectorHook
 } from 'react-redux';
-import authReducer from './authSlice';
 
-// const rootReducer = () => {}; // Заменить на импорт настоящего редьюсера
+const rootReducer = () => {}; // Заменить на импорт настоящего редьюсера
 
-export const rootReducer = {
-  auth: authReducer // Включаем authReducer в корневой редуктор
-};
-
-export const store = configureStore({
+const store = configureStore({
   reducer: rootReducer,
   devTools: process.env.NODE_ENV !== 'production'
 });
 
-export type RootState = ReturnType<typeof store.getState>;
+export type RootState = ReturnType<typeof rootReducer>;
+
 export type AppDispatch = typeof store.dispatch;
 
 export const useDispatch: () => AppDispatch = () => dispatchHook();
 export const useSelector: TypedUseSelectorHook<RootState> = selectorHook;
 
 export default store;
-
-// const store = configureStore({
-//   reducer: rootReducer,
-//   devTools: process.env.NODE_ENV !== 'production'
-// });
-
-// export type RootState = ReturnType<typeof rootReducer>;
-
-// export type AppDispatch = typeof store.dispatch;
-
-// export const useDispatch: () => AppDispatch = () => dispatchHook();
-// export const useSelector: TypedUseSelectorHook<RootState> = selectorHook;
-
-// export default store;
